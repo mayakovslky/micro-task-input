3ca760b1ad0a8d3f17444ee05e643e4844cde2e8
mayakovsky <mayakovsky03@gmail.com>
2022-09-04 16:20:04 +0300

    add FullInput

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,36 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Fullinput} from "./components/Fullinput";
 
 function App() {
+  const [message, setMessage] = useState([
+        {message: 'message1'},
+        {message: 'message2'},
+        {message: 'message3'},
+        {message: 'message4'},
+        {message: 'message5'}
+      ]
+  )
+
+    const addMessage=(title:string)=>{
+        let newMessage = {message: title};
+        setMessage([newMessage,...message])
+    }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        {/*<div>*/}
+        {/*  <input />*/}
+        {/*  <button>+</button>*/}
+        {/*</div>*/}
+          <Fullinput addMessage={addMessage}/>
+        {message.map((el, index) => {
+          return (
+              <div key={index}>{el.message}</div>
+          )
+        })}
+      </div>
   );
 }
 
