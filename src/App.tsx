be13a36bbc8acc1655ad6db1e5651a5d659a3ae6
mayakovsky <mayakovsky03@gmail.com>
2022-09-04 17:14:53 +0300

    add Input and Button

--- src/App.tsx
+++ src/App.tsx
@@ -2,36 +2,47 @@ import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import {Fullinput} from "./components/Fullinput";
+import {Input} from "./components/Input";
+import {Button} from "./components/Button";
 
 function App() {
-  const [message, setMessage] = useState([
-        {message: 'message1'},
-        {message: 'message2'},
-        {message: 'message3'},
-        {message: 'message4'},
-        {message: 'message5'}
-      ]
-  )
+    const [message, setMessage] = useState([
+            {message: 'message1'},
+            {message: 'message2'},
+            {message: 'message3'},
+            {message: 'message4'},
+            {message: 'message5'}
+        ]
+    )
 
-    const addMessage=(title:string)=>{
+    let [title, setTitle] = useState('')
+
+    const addMessage = (title: string) => {
         let newMessage = {message: title};
-        setMessage([newMessage,...message])
+        setMessage([newMessage, ...message])
     }
 
-  return (
-      <div className="App">
-        {/*<div>*/}
-        {/*  <input />*/}
-        {/*  <button>+</button>*/}
-        {/*</div>*/}
-          <Fullinput addMessage={addMessage}/>
-        {message.map((el, index) => {
-          return (
-              <div key={index}>{el.message}</div>
-          )
-        })}
-      </div>
-  );
+    const callBackButtonHandler = () => {
+        addMessage(title)
+        setTitle('')
+    }
+
+    return (
+        <div className="App">
+            {/*<div>*/}
+            {/*  <input />*/}
+            {/*  <button>+</button>*/}
+            {/*</div>*/}
+            {/*  <Fullinput addMessage={addMessage}/>*/}
+            <Input setTitle={setTitle} title={title}/>
+            <Button name={'+'} callBack={callBackButtonHandler}/>
+            {message.map((el, index) => {
+                return (
+                    <div key={index}>{el.message}</div>
+                )
+            })}
+        </div>
+    );
 }
 
 export default App;
